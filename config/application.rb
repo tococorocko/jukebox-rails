11929e414c66186e409f7af5766413071aaa5134
Lucca Kaiser <lucca.kaiser@justrelate.com>
2022-08-08 20:25:12 +0200

    set timezone

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module JukeboxRails
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Zurich"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
