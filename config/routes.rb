d7f1b1b7ae96a829b1f712ca4f25d04410ba4e2b
Lucca Kaiser <lucca.kaiser@justrelate.com>
2022-08-10 22:17:28 +0200

    add temporary dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   resources :jukeboxes
 
@@ -16,4 +18,6 @@ Rails.application.routes.draw do
   get 'queue/:jukebox_id', action: :queue, controller: 'jukeboxes'
   get 'next-song/:jukebox_id', action: :next_song, controller: 'jukeboxes'
   post 'take-photo/:jukebox_id', action: :take_photo, controller: 'jukeboxes'
+
+  mount Sidekiq::Web => "/sidekiq"
 end
